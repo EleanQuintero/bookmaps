c20e83ebf1995d801fa7fc4ff68fb19b93cf10be
EleanQuintero <elean.quintero.madrid@gmail.com>
2026-01-02 23:33:01 +0100

    controllers and services names modified

---
+++ src/services/authService.ts
@@ -0,0 +1,61 @@
+import { loginUser, newUser } from "@/domain/entities/user"
+import { createClient } from "@/lib/supabase/server"
+
+
+export async function signUp({ username, email, password }: newUser) {
+
+    const supabase = await createClient()
+
+
+    const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+            data: {
+                username: username
+            }
+        }
+    })
+
+    if (error) {
+        return { data: null, error: error.message }
+    }
+
+    return { data, error: null }
+}
+
+export async function signIn({ email, password }: loginUser) {
+
+    const supabase = await createClient()
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+        options: {
+
+        }
+    })
+
+    if (error) {
+        return { data: null, error: error.message }
+    }
+
+    return { data, error: null }
+
+}
+
+export async function signOut() {
+    const supabase = await createClient()
+
+    const { error } = await supabase.auth.signOut()
+    return error
+
+}
+
+export async function getCurrentUserData() {
+    const supabase = await createClient()
+
+    const currentUserData = await supabase.auth.getUser()
+
+    return currentUserData
+}
