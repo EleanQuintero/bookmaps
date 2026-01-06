f10119809dcf0d5e3809639ee14061e3b2930b1a
EleanQuintero <elean.quintero.madrid@gmail.com>
2026-01-06 11:59:47 +0100

    changed folder for  auth controller / service

---
+++ src/controllers/auth/authController.ts
@@ -0,0 +1,34 @@
+import { loginUser, newUser } from "@/domain/entities/users/user";
+import { signUp, signIn, getCurrentUserData, signOut } from "@/services/auth/authService";
+
+export async function createUser({ username, email, password }: newUser) {
+    const result = await signUp({ username, email, password })
+    return result
+}
+
+export async function signInUser({ email, password }: loginUser) {
+    const result = await signIn({ email, password })
+    return result
+
+}
+
+
+export async function signOutUser() {
+    const error = await signOut()
+
+    if (error) return error
+
+
+}
+
+
+export async function getCurrentUser() {
+    const { data } = await getCurrentUserData()
+
+    const user = {
+        id: data.user?.id,
+        email: data.user?.email,
+        username: data.user?.user_metadata.username
+    }
+    return user
+} 
