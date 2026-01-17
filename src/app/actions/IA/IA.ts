c84389f09cd122d648fee6b5cdaa94c00d14aa10
EleanQuintero <elean.quintero.madrid@gmail.com>
2026-01-17 15:32:28 +0100

    feat: updated types from zod

--- src/app/actions/IA/IA.ts
+++ src/app/actions/IA/IA.ts
@@ -1,19 +1,9 @@
 "use server"
-import { iaBookmapData } from "@/domain/entities/maps/maps";
+import { AIMapResponse } from "@/domain/schemes/maps/bookmap-scheme";
 import { aiGeneratorService } from "@/services/IA/maps/bookMapGenService";
-import { createBookmap } from "@/services/IA/mocks/iaBookService";
 
 
-
-export async function getMockIABookmapData(theme: string): Promise<iaBookmapData> {
-
-    const data = await createBookmap(theme);
-
-    return data;
-
-}
-
-export async function getBookMap(theme: string): Promise<iaBookmapData> {
+export async function getBookMap(theme: string): Promise<AIMapResponse> {
 
     const generator = aiGeneratorService
 
