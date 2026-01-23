2aa4a13d222c9992a315a6799050fa408bea5f7a
EleanQuintero <elean.quintero.madrid@gmail.com>
2026-01-23 13:30:07 +0100

    feat: updated bookinfo component

--- src/app/dashboard/components/client/BookInfo.tsx
+++ src/app/dashboard/components/client/BookInfo.tsx
@@ -11,6 +11,8 @@ import {
 } from "lucide-react";
 import { BookItemProps } from "../BookItem";
 import { useState, type ReactNode } from "react";
+import { BookStatus } from "@/domain/entities/models/models";
+import { useUpdateBookStatus } from "@/hooks/querys/use-update";
 
 interface BookInfoProps extends Omit<
   BookItemProps,
@@ -26,21 +28,51 @@ export function BookInfo({
   status,
   position,
   children,
+  isbn,
 }: BookInfoProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const [bookStatus, setBookStatus] = useState<BookStatus>(status);
+
+  const { updateStatus, isPending } = useUpdateBookStatus();
 
   function handleToggleNotes() {
     setIsOpen(!isOpen);
+  }
+
+  function updateBookStatus() {
+    const prevState = bookStatus;
+
+    const nextStatusMap: Record<BookStatus, BookStatus> = {
+      to_read: "reading",
+      reading: "completed",
+      completed: "to_read",
+    };
+
+    const nextStatus = nextStatusMap[bookStatus];
+
+    updateStatus(
+      { id: isbn, status: nextStatus },
+      {
+        onError: () => {
+          setBookStatus(prevState);
+        },
+      },
+    );
+    setBookStatus(nextStatus);
   }
 
   return (
     <div className="flex-1 min-w-0">
       <div className="flex items-start justify-between gap-4 mb-3">
         <div className="flex items-center gap-3">
-          <button className="focus:outline-none shrink-0 hover:scale-110 transition-transform">
-            {status === "completed" ? (
+          <button
+            onClick={updateBookStatus}
+            className="focus:outline-none shrink-0 hover:scale-110 transition-transform"
+            disabled={isPending}
+          >
+            {bookStatus === "completed" ? (
               <CheckCircle2 className="h-7 w-7 text-green-500" />
-            ) : status === "reading" ? (
+            ) : bookStatus === "reading" ? (
               <Clock className="h-7 w-7 text-orange-500" />
             ) : (
               <Circle className="h-7 w-7 text-muted-foreground" />
@@ -51,19 +83,19 @@ export function BookInfo({
               <span className="text-xs font-semibold text-muted-foreground uppercase tracking-wide">
                 Step {position}
               </span>
-              {status === "reading" && (
+              {bookStatus === "reading" && (
                 <Badge variant="warning" className="text-xs">
                   Reading Now
                 </Badge>
               )}
-              {status === "completed" && (
+              {bookStatus === "completed" && (
                 <Badge variant="success" className="text-xs">
                   Completed
                 </Badge>
               )}
             </div>
             <h3
-              className={`font-bold text-xl mb-1 ${status === "completed" ? "text-muted-foreground" : ""}`}
+              className={`font-bold text-xl mb-1 ${bookStatus === "completed" ? "text-muted-foreground" : ""}`}
             >
               {title}
             </h3>
@@ -94,7 +126,7 @@ export function BookInfo({
       <div className="flex items-center gap-4 text-xs text-muted-foreground">
         <div className="flex items-center gap-1">
           <div
-            className={`h-2 w-2 rounded-full ${status === "completed" ? "bg-green-500" : status === "reading" ? "bg-orange-500" : "bg-muted-foreground"}`}
+            className={`h-2 w-2 rounded-full ${bookStatus === "completed" ? "bg-green-500" : bookStatus === "reading" ? "bg-orange-500" : "bg-muted-foreground"}`}
           />
         </div>
       </div>
