b7882dccc493a4583f671c016f94d55a37af8ebe
EleanQuintero <elean.quintero.madrid@gmail.com>
2026-02-02 00:51:02 +0100

    feat: added toaster for toast notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import TanStackProvider from "@/providers/query-provider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <TanStackProvider>{children}</TanStackProvider>
+        <Toaster />
       </body>
     </html>
   );
